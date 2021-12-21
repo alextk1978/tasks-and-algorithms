22d04c783b46bd380f40cfd97d520a2c3ec0aaf3
Алексей Ткаченко <alextk2017@gmail.com>
2021-12-21 11:10:28 +0300

    Добавлена задача Kata

--- src/tasks/AreTheyTheSame.java
+++ src/tasks/AreTheyTheSame.java
@@ -8,9 +8,10 @@ import java.util.Arrays;
 /**
  * @author Alexey Tkachenko
  * <p>
- * Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements,
- * with the same multiplicities (the multiplicity of a member is the number of times it appears).
- * "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+ * Для двух массивов a и b напишите функцию comp (a, b) (orcompSame (a, b)), которая проверяет,
+ * имеют ли два массива «одинаковые» элементы
+ * с одинаковой кратностью (кратность члена - это количество раз, которое он встречается).
+ * «одинаковые» здесь означает, что элементы в b являются элементами a в квадрате, независимо от порядка.
  * <p>
  * Examples
  * Valid arrays
